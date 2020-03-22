df3adfb194068287e65f8a90cbcb58798fd615bf
romanakhatun <romanakhatun2004@gmail.com>
2020-03-22 21:19:22 +0600

    secound

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div className="cart">
+            <div className="deleveryDetails">
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Cart;
